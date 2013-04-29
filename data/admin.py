aa5e3e53478f4b23e50b05371eb2115b4cafcdc3
Govind S Menokee <govindsmenokee@gmail.com>
2013-04-29 18:07:30 +0530

    imported the database and added grappelli admin module!

---
+++ data/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from data.models import DDC,Author,Book,Title_word
+
+admin.site.register(DDC)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Title_word)
