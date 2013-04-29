2149469c2cd05bf2d304bfd19068b6a501afddcd
Govind S Menokee <govindsmenokee@gmail.com>
2013-04-30 00:51:07 +0530

    displayed the results and paginated them!.

--- grandham/urls.py
+++ grandham/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'grandham.views.home', name='home'),
+    url(r'^test$', 'data.views.test', name='test'),
     # url(r'^grandham/', include('grandham.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
